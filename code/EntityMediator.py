899081a1d9dddb7c69d56e5c624692e154c1ad8f
LeticiaGoulart <leticiagoulart098@gmail.com>
2025-08-28 16:40:57 -0300

    Creating Player Shot Creating Enemy Shot

--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,4 +1,6 @@
-from code import entity
+from code.Const import WIN_WIDTH
+from code.EnemyShot import EnemyShot
+from code.PlayerShot import PlayerShot
 from code.enemy import Enemy
 from code.entity import Entity
 
@@ -8,19 +10,22 @@ class EntityMediator:
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.rect.right < 0:
+            if ent.rect.right <= 0:
                 ent.health = 0
-        pass
-
-
-    @staticmethod
-    def verify_collision(entity_list: list [Entity]):
-        for i in range(len(entity_list)):
-            test_entity = entity_list[i]
-            EntityMediator.__verify_collision_window(test_entity)
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
+            if ent.rect.right <= 0:
+                ent.health = 0
 
     @staticmethod
-    def verify_health(entity_list: list [Entity]):
+    def verify_collision(entity_list: list[Entity]):
+        for ent in entity_list:
+            EntityMediator.__verify_collision_window(ent)
+
+    @staticmethod
+    def verify_health(entity_list: list[Entity]):
         for ent in entity_list:
             if ent.health <= 0:
                 entity_list.remove(ent)
